1a3f6036154bcb6b8333d25af3019094b4fab30f
Kamil Domian <kamdomian@gmail.com>
2023-06-26 17:02:10 +0200

    Dodano instrukcje kolejnych krokow

---
+++ src/main/java/org/example/Aplication.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Aplication {
+    public void viewMenu(){
+        System.out.println("Menu");
+        System.out.println("Motocykl");
+        System.out.println("Samochód");
+        System.out.println("Ciężarówka");
+
+    }
+    public Integer getUserChoice(String info){
+        System.out.println(info);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+
+
+
+
+    }
+
+
+}
